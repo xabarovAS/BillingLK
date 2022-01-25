bedd2e3b15785b1bb686113e4e2659877fa87412
xabarovAS <xabarov.a.s@astra-best.ru>
2022-01-25 13:38:05 +0400

    Отделил последовательные и параллельные операции

---
+++ src/main/java/ru/BillingLK/BillingLK/ExchangeBillingAPI/ParallelOperations/Payments/entity/Payment.java
@@ -0,0 +1,7 @@
+package ru.BillingLK.BillingLK.ExchangeBillingAPI.ParallelOperations.Payments.entity;
+
+public class Payment {
+    private String PersonalAccountID;
+    private int Value;
+    private String PaymentID;
+}
