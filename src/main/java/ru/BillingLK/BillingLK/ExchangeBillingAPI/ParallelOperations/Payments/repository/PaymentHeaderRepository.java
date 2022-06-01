87986b7835310f0d2d2c4c14817835b887ffcdf3
xabarov.a.s <bboyworld933@gmail.com>
2022-06-01 14:50:06 +0400

    Change type repo

--- src/main/java/ru/BillingLK/BillingLK/ExchangeBillingAPI/ParallelOperations/Payments/repository/PaymentHeaderRepository.java
+++ src/main/java/ru/BillingLK/BillingLK/ExchangeBillingAPI/ParallelOperations/Payments/repository/PaymentHeaderRepository.java
@@ -1,7 +1,8 @@
 package ru.BillingLK.BillingLK.ExchangeBillingAPI.ParallelOperations.Payments.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.BillingLK.BillingLK.ExchangeBillingAPI.ParallelOperations.Payments.entity.PaymentHeader;
 
-public interface PaymentHeaderRepository extends CrudRepository<PaymentHeader, String> {
+public interface PaymentHeaderRepository extends JpaRepository<PaymentHeader, String> {
 }
--- src/main/java/ru/BillingLK/BillingLK/ExchangeBillingAPI/ParallelOperations/Payments/repository/PaymentRepository.java
+++ src/main/java/ru/BillingLK/BillingLK/ExchangeBillingAPI/ParallelOperations/Payments/repository/PaymentRepository.java
@@ -1,12 +1,13 @@
 package ru.BillingLK.BillingLK.ExchangeBillingAPI.ParallelOperations.Payments.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import ru.BillingLK.BillingLK.ExchangeBillingAPI.ParallelOperations.Payments.entity.Payment;
 
-public interface PaymentRepository extends CrudRepository<Payment, String> {
+public interface PaymentRepository extends JpaRepository<Payment, String> {
 
     @Modifying
     @Query("delete from Payment p where p.objectBaseID=:objectBaseID")
